b4d3f18f3bb497edeb193778ad7239fa4caf9ab9
starcolon <tao@starcolon.com>
2018-02-17 03:31:52 +0700

    (poc) seq from cols

--- common/src/main/scala/com/starcolon/ml/DatasetUtils.scala
+++ common/src/main/scala/com/starcolon/ml/DatasetUtils.scala
@@ -3,6 +3,7 @@ package com.starcolon.ml
 import org.apache.spark.sql.{SparkSession, Dataset, Row}
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
+import scala.reflect.runtime.universe._
 
 object DatasetUtils {
 
@@ -25,6 +26,38 @@ object DatasetUtils {
 
     def dropMultiple(cols: Seq[String]): Dataset[Row] = 
       cols.foldLeft(df){case(d,c) => d.drop(c)}
+
+    def seqFromColumns(cols: Seq[String], target: String): RDD[Seq[T]] = {
+      val t = df.schema(cols.head).dataType
+      require(cols.tail.map(c => df.schema(c).dataType == t).reduce(_ && _),
+        s"All columns (${cols.mkString(", ")}) have to be of the same type.")
+      
+      // TAOTODO:
+      val initialIntArray = udf{() => Seq.empty[Int]}
+      val initialLongArray = udf{() => Seq.empty[Long]}
+      val initialDoubleArray = udf{() => Seq.empty[Double]}
+      val initialStringArray = udf{() => Seq.empty[String]}
+
+      val appendIntArray = udf{(i: Int, ns: Seq[Int]) => ns :+ i}
+      val appendLongArray = udf{(i: Long, ns: Seq[Long]) => ns :+ i}
+      val appendDoubleArray = udf{(i: Double, ns: Seq[Double]) => ns :+ i}
+      val appendStringArray = udf{(i: String, ns: Seq[String]) => ns :+ i}
+
+      val df_ = t match {
+        case IntType => df.schema df.withColumn(target, initialIntArray)
+        case LongType => df.schema df.withColumn(target, initialLongArray)
+        case DoubleType => df.schema df.withColumn(target, initialDoubleArray)
+        case StringType => df.schema df.withColumn(target, initialStringArray)
+        case _ => throw IllegalArgumentException(s"Unsupported type : $t")
+      }
+
+      cols.foldLeft(df_){ case(c,d) => 
+        
+
+      }
+    }
+
+    def bucket(bins: Seq[Double]): Dataset[Row] = ???
   }
 
 }
