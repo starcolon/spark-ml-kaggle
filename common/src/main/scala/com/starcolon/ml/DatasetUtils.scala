457fd7531aead375f0870495a5a3a4e0e298b098
starcolon <tao@starcolon.com>
2018-02-17 03:50:36 +0700

    (wip) seg of cols

--- common/src/main/scala/com/starcolon/ml/DatasetUtils.scala
+++ common/src/main/scala/com/starcolon/ml/DatasetUtils.scala
@@ -7,6 +7,16 @@ import scala.reflect.runtime.universe._
 
 object DatasetUtils {
 
+  private[ml] val initialIntArray = udf{() => Seq.empty[Int]}
+  private[ml] val initialLongArray = udf{() => Seq.empty[Long]}
+  private[ml] val initialDoubleArray = udf{() => Seq.empty[Double]}
+  private[ml] val initialStringArray = udf{() => Seq.empty[String]}
+
+  private[ml] val appendIntArray = udf{(i: Int, ns: Seq[Int]) => ns :+ i}
+  private[ml] val appendLongArray = udf{(i: Long, ns: Seq[Long]) => ns :+ i}
+  private[ml] val appendDoubleArray = udf{(i: Double, ns: Seq[Double]) => ns :+ i}
+  private[ml] val appendStringArray = udf{(i: String, ns: Seq[String]) => ns :+ i}
+
   private val toOption = udf((v: String, _null: String) => {
     if (v==_null) None 
     else Some(v)
@@ -27,33 +37,26 @@ object DatasetUtils {
     def dropMultiple(cols: Seq[String]): Dataset[Row] = 
       cols.foldLeft(df){case(d,c) => d.drop(c)}
 
-    def seqFromColumns(cols: Seq[String], target: String): RDD[Seq[T]] = {
+    def seqFromColumns(cols: Seq[String], target: String): Dataset[Row] = {
       val t = df.schema(cols.head).dataType
       require(cols.tail.map(c => df.schema(c).dataType == t).reduce(_ && _),
         s"All columns (${cols.mkString(", ")}) have to be of the same type.")
-      
-      // TAOTODO:
-      val initialIntArray = udf{() => Seq.empty[Int]}
-      val initialLongArray = udf{() => Seq.empty[Long]}
-      val initialDoubleArray = udf{() => Seq.empty[Double]}
-      val initialStringArray = udf{() => Seq.empty[String]}
-
-      val appendIntArray = udf{(i: Int, ns: Seq[Int]) => ns :+ i}
-      val appendLongArray = udf{(i: Long, ns: Seq[Long]) => ns :+ i}
-      val appendDoubleArray = udf{(i: Double, ns: Seq[Double]) => ns :+ i}
-      val appendStringArray = udf{(i: String, ns: Seq[String]) => ns :+ i}
 
       val df_ = t match {
-        case IntType => df.schema df.withColumn(target, initialIntArray)
-        case LongType => df.schema df.withColumn(target, initialLongArray)
-        case DoubleType => df.schema df.withColumn(target, initialDoubleArray)
-        case StringType => df.schema df.withColumn(target, initialStringArray)
-        case _ => throw IllegalArgumentException(s"Unsupported type : $t")
+        case IntegerType => df.withColumn(target, initialIntArray())
+        case LongType => df.withColumn(target, initialLongArray())
+        case DoubleType => df.withColumn(target, initialDoubleArray())
+        case StringType => df.withColumn(target, initialStringArray())
+        case _ => throw new java.lang.IllegalArgumentException(s"Unsupported type : $t")
       }
 
-      cols.foldLeft(df_){ case(c,d) => 
-        
-
+      cols.foldLeft(df_){ case(d,c) => 
+        t match {
+          case IntegerType => d.withColumn(target, appendIntArray(col(c), col(target)))
+          case LongType => d.withColumn(target, appendLongArray(col(c), col(target)))
+          case DoubleType => d.withColumn(target, appendDoubleArray(col(c), col(target)))
+          case StringType => d.withColumn(target, appendStringArray(col(c), col(target)))
+        }
       }
     }
 
