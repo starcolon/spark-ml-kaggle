9bfc5bd22169b0dd7c45d36c1566f82fc248aecc
starcolon <tao@starcolon.com>
2018-02-17 13:32:19 +0700

    all tests for dataset utils

--- common/src/test/scala/com/starcolon/ml/DatasetUtilTest.scala
+++ common/src/test/scala/com/starcolon/ml/DatasetUtilTest.scala
@@ -32,11 +32,36 @@ class DatasetUtilTest extends SparkTestInstance with Matchers {
     lazy val bb = B("a","bb","ccc") :: B("a","b","c") :: B("1","2","3") :: Nil
     lazy val cc = C(3.4, 4.5, 5.6) :: C(0,0,0) :: C(0,1.5,5.1) :: Nil
 
-    it("should create a column of array (of Integer) from multiple columns"){
+    lazy val dfA = aa.toDF
+    lazy val dfB = bb.toDF
+    lazy val dfC = cc.toDF
 
+    it("should create a column of array (of Integer) from multiple columns"){
+      val nn = dfA
+        .seqFromColumns(Seq("i","j","k"), "x")
+        .select("x")
+        .rdd.map{_.getAs[Seq[Int]](0)}.collect
+
+      nn should contain allOf(Seq(1,2,3), Seq(0,1,1), Seq(3,4,5), Seq(0,-1,-1))
     }
 
+    it("should create a column of array (of String) from multiple columns"){
+      val nn = dfB
+        .seqFromColumns(Seq("i","j","k"), "x")
+        .select("x")
+        .rdd.map{_.getAs[Seq[String]](0)}.collect
 
+      nn should contain allOf(Seq("a","bb","ccc"), Seq("a","b","c"), Seq("1","2","3"))
+    }
+
+    it("should create a column of array (of Double) from multiple columns"){
+      val nn = dfC
+        .seqFromColumns(Seq("i","j","k"), "x")
+        .select("x")
+        .rdd.map{_.getAs[Seq[Double]](0)}.collect
+
+      nn should contain allOf(Seq(3.4, 4.5, 5.6), Seq(0,0,0), Seq(0,1.5,5.1))
+    }
   }
 
 }
